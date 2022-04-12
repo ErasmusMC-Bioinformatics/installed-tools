7b813aa3a459098c6782494d16faedbb7a6eea83
Saskia Hiltemann <zazkia@gmail.com>
2022-04-12 14:16:33 +0200

    fix hostname

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -18,14 +18,14 @@ pip install -r requirements.txt
 make fix
 
 # Apply the changes
-make install GALAXY_SERVER=http://bioinf-galaxy.erasmusmc.nl/ GALAXY_API_KEY=$GALAXY_ADMIN_API_KEY
+make install GALAXY_SERVER=http://bioinf-galactus.erasmusmc.nl/ GALAXY_API_KEY=$GALAXY_ADMIN_API_KEY
 
 # Install workflows
-find . -name '*.ga' -exec workflow-install -g http://bioinf-galaxy.erasmusmc.nl/ -a $GALAXY_ADMIN_API_KEY -w '{}' \;
+find . -name '*.ga' -exec workflow-install -g http://bioinf-galactus.erasmusmc.nl/ -a $GALAXY_ADMIN_API_KEY -w '{}' \;
 
 # Reload the toolbox a couple times.
-curl -X PUT http://bioinf-galaxy.erasmusmc.nl/api/configuration/toolbox -H "x-api-key: $GALAXY_ADMIN_API_KEY"
+curl -X PUT http://bioinf-galactus.erasmusmc.nl/api/configuration/toolbox -H "x-api-key: $GALAXY_ADMIN_API_KEY"
 sleep 2
-curl -X PUT http://bioinf-galaxy.erasmusmc.nl/api/configuration/toolbox -H "x-api-key: $GALAXY_ADMIN_API_KEY"
+curl -X PUT http://bioinf-galactus.erasmusmc.nl/api/configuration/toolbox -H "x-api-key: $GALAXY_ADMIN_API_KEY"
 sleep 2
-curl -X PUT http://bioinf-galaxy.erasmusmc.nl/api/configuration/toolbox -H "x-api-key: $GALAXY_ADMIN_API_KEY"
+curl -X PUT http://bioinf-galactus.erasmusmc.nl/api/configuration/toolbox -H "x-api-key: $GALAXY_ADMIN_API_KEY"
